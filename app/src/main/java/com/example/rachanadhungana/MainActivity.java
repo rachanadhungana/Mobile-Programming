1a6cada8cfd87213cba81e7a4a03a97cc4e7965e
rachanadhungana <rachanadhungana88@gmail.com>
2025-11-10 09:00:22 +0545

    Mobile Programming

--- app/src/main/java/com/example/rachanadhungana/MainActivity.java
+++ app/src/main/java/com/example/rachanadhungana/MainActivity.java
@@ -3,17 +3,21 @@ package com.example.rachanadhungana;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
+import androidx.appcompat.widget.AppCompatSpinner;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
     private AppCompatButton btnRegister;
+    private AppCompatSpinner spinnerGender;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this,"Email: "+email,Toast.LENGTH_SHORT).show();
 
         btnRegister=findViewById(R.id.btnRegister);
+        spinnerGender=findViewById(R.id.spinnerGender);
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -33,5 +38,22 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+    private void setSpinnerGender(){
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,getResources().getStringArray(R.array.genderValue));
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerGender.setAdapter(adapter);
+        spinnerGender.setSelection(0);
+        spinnerGender.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this,parent.getItemAtPosition(position).toString(),Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 }
