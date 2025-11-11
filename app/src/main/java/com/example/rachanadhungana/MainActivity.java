6ad951988f88aa5e7560436dbf2f518f10ce5ef5
rachanadhungana <rachanadhungana88@gmail.com>
2025-11-11 07:50:03 +0545

    2082/07/25

--- app/src/main/java/com/example/rachanadhungana/MainActivity.java
+++ app/src/main/java/com/example/rachanadhungana/MainActivity.java
@@ -5,9 +5,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 import androidx.appcompat.widget.AppCompatSpinner;
@@ -18,6 +21,7 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
     private AppCompatButton btnRegister;
     private AppCompatSpinner spinnerGender;
+    private RadioGroup rgGender;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +32,9 @@ public class MainActivity extends AppCompatActivity {
 
         btnRegister=findViewById(R.id.btnRegister);
         spinnerGender=findViewById(R.id.spinnerGender);
+        rgGender=findViewById(R.id.rgGender);
+        setSpinnerGender();
+        setRadioGroup();
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -38,6 +45,15 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+    private void setRadioGroup(){
+        rgGender.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(@NonNull RadioGroup group, int checkedId) {
+                RadioButton selectedRadioButton=findViewById(checkedId);
+                Toast.makeText(MainActivity.this,selectedRadioButton.getText().toString(),Toast.LENGTH_SHORT).show();
+            }
+        });
     }
     private void setSpinnerGender(){
         ArrayAdapter<String> adapter=new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,getResources().getStringArray(R.array.genderValue));
