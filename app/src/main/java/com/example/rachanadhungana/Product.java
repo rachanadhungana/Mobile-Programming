00c63b9300d0abeb6ddebb893b01275391249e0a
rachanadhungana <rachanadhungana88@gmail.com>
2025-12-07 07:46:44 +0545

    ListView

---
+++ app/src/main/java/com/example/rachanadhungana/Product.java
@@ -0,0 +1,58 @@
+package com.example.rachanadhungana;
+
+public class Product {
+    int imageProduct;
+    String title;
+    String productDescription;
+    String price;
+    String unit;
+    String quantity;
+
+    public int getImageProduct() {
+        return imageProduct;
+    }
+
+    public void setImageProduct(int imageProduct) {
+        this.imageProduct = imageProduct;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getProductDescription() {
+        return productDescription;
+    }
+
+    public void setProductDescription(String productDescription) {
+        this.productDescription = productDescription;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+}
