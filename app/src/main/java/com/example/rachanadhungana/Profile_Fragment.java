bfbee1538cd35985be07a5e8168c7aa9a8c439ac
rachanadhungana <rachanadhungana88@gmail.com>
2025-12-07 07:31:03 +0545

    AlertBox

--- app/src/main/java/com/example/rachanadhungana/Profile_Fragment.java
+++ app/src/main/java/com/example/rachanadhungana/Profile_Fragment.java
@@ -1,5 +1,7 @@
 package com.example.rachanadhungana;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,6 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 
 public class Profile_Fragment extends Fragment {
@@ -30,5 +33,32 @@ public class Profile_Fragment extends Fragment {
     }
     private void findView(){
         btnAlert=mainView.findViewById(R.id.btnAlert);
+        btnAlert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createAlertDialog();
+            }
+        });
+    }
+
+    private void createAlertDialog(){
+        AlertDialog.Builder builder=new AlertDialog.Builder(getContext());
+        builder.setTitle("Alert");
+        builder.setMessage("This is an Alert Box");
+        builder.setIcon(R.drawable.user);
+        builder.setPositiveButton("Okay", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getContext(),"Okay is clicked",Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getContext(),"Cancel is clicked",Toast.LENGTH_SHORT).show();
+            }
+        });
+        AlertDialog dialog= builder.create();
+        dialog.show();
     }
 }
