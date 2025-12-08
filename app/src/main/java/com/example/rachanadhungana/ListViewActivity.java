ec74c88ea1a964830be02e3c75701648af8fc8af
Stack-Nova <jineshbasnet6@gmail.com>
2025-12-08 08:21:49 +0545

    adapter addded productlistadapter,adapterlistitem,

---
+++ app/src/main/java/com/example/rachanadhungana/ListViewActivity.java
@@ -0,0 +1,19 @@
+package com.example.rachanadhungana;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ListViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_view);
+
+        };
+    }
